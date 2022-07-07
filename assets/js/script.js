901e3f7688ac4cf111dc47a789f65070001e24dc
night <yseddiq@gmail.com>
2022-07-06 23:44:11 -0400

    cleaned up some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -299,60 +299,24 @@ var saveTasks = function(){
 
 var loadTasks = function(){
 
-    tasks = localStorage.getItem("tasks", tasks);
+    bobTasks = localStorage.getItem("tasks");
 
     //console.log(savedTasks);
 
-    if(!tasks){
+    if(!bobTasks){
 
         return false;
     }
 
-    tasks = JSON.parse(tasks);
+    bobTasks = JSON.parse(bobTasks);
     //console.log(tasks);
 
-    for( var i =0;i < tasks.length;i++){
+    //loop through saved tasks array
 
-        tasks[i].id =taskIdCounter;
-        var listItemEl =  document.createElement("li");
-            listItemEl.className="task-item";
-            listItemEl.setAttribute("data-task-id", tasks[i].id)    
-        var taskInfoEl = document.createElement("div");
-            taskInfoEl.className="task-info";
-            taskInfoEl.innerHTML= "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    for(var i = 0;i<bobTasks.length;i++){
 
-        listItemEl.appendChild(taskInfoEl);
-        
-        
-        var capuletAction = createTaskActions(tasks[i].id)  
-
-        listItemEl.appendChild(capuletAction);
-        
-        if(tasks[i].status==="to do"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex=0;
-
-
-            tasksToDoEl.appendChild(listItemEl);
-
-        }
-
-        else if(tasks[i].status==="in progress"){
-
-            listItemEl.querySelector("select[name='status-change']").selectedIndex=1
-            tasksInProgressEl.appendChild(listItemEl);
-
-        }
-
-        else if (tasks[i].status==="complete"){
-
-            listItemEl.querySelector("select[name='status-change']").selectedIndex=2
-            tasksCompletedEl.appendChild(listItemEl);
-
-        }
-
-        taskIdCounter++;
-        console.log(listItemEl);
-
+        //pass each task object to the createTaskEl function
+        createTaskEl(bobTasks[i])
 
     }
 
